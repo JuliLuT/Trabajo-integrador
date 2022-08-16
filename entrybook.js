b7d7010b75f4fc74c56c2531b7b252c74ad5651f
Juan Roberts <juancroberts31@gmail.com>
2022-08-16 16:45:16 -0300

    Correción TP Integrador.
    Opción default en tu entrybook.js

--- entrybook.js
+++ entrybook.js
@@ -3,6 +3,8 @@ const lectura = require('./options/lectura')
 const edicion = require('./options/edicion')
 const borrado = require('./options/borrado')
 
+// Es una buena práctica añadir una opción default a tu switch, en este caso que al menos imprima en consola una string indicando que el comando pasado no fue válido.
+
 switch (process.argv[2]) {
     case 'new':
         carga.carga(process.argv[3], process.argv[4], process.argv[5], process.argv[6], process.argv[7], process.argv[8])
